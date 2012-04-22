b3593c97ec50974ea9a45640fd138e6bd2b50391
murkey <murkey@freqbase.com>
2012-04-21 21:25:42 -0700

    finished: generate fibonacci word dynamically

--- rabbit-fractal.js
+++ rabbit-fractal.js
@@ -1,15 +1,26 @@
 var interval;
 var index = 0;
 var word = '';
+console.log("MUFUCKA " + word.length);
+
+var S = ['0','1'];
+
+var generateFibonacciWord = function() {
+  console.log('generating path...');
+  word = S[0] + S[1]
+  S[1] = S[0];
+  S[0] = word;
+
+  console.log(word);
+}
 
 var draw = function() {
   index++;
   if (index > word.length) {
-    console.log('DONE! completed ' + (index + 1) + ' instructions');
-    return;
+    generateFibonacciWord();
   }
   
-  if (word[index] == "0") {
+  if (word[index - 1] == "0") {
     if (index % 2 == 0) {
       ctx.rotate(Math.PI/2);
     } else {
@@ -28,16 +39,6 @@ var draw = function() {
 console.log('this will totally be awesome');
 
 var ctx = document.getElementById('canvas').getContext('2d');
-var S = ['0','1'];
-
-console.log('generating path...');
-for (var i = 0; i < 27; i++) {
-  word = S[0] + S[1]
-  S[1] = S[0];
-  S[0] = word;
-}
-
-console.log(word);
 console.log('rendering...');
 ctx.strokeStyle = '#ffffff';
 ctx.translate(20,20);
